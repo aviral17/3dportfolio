5d9db5944556e1591fb9c2d3acc92e975ecd3972
Aviral <36530173+aviral17@users.noreply.github.com>
2025-04-19 22:37:08 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
   const [showApp, setShowApp] = useState(false);
 
   const experienceRef = useRef(null);
-
+// update 3D model and functions
   return (
     <>
       <SplashScreen onFinished={() => setShowApp(true)} />
